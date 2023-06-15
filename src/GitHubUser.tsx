4aeba6d75689058d3d78ed0521fb895dc774b6f7
Adrian Burgos <adrian.burgos.colas@gmail.com>
2023-06-15 18:32:02 -0400

    FEAT: added spinner and user card to GitHubUser comp

--- src/GitHubUser.tsx
+++ src/GitHubUser.tsx
@@ -1,6 +1,12 @@
+import { Suspense } from "react";
+
+import GitHubUserCard from "./GitHubUser/GitHubUserCard";
+import Spinner from "./GitHubUser/Spinner";
+
 export default function GitHubUser() {
   return (
-    <section>
-    </section>
+    <Suspense fallback={<Spinner />}>
+      <GitHubUserCard />
+    </Suspense>
   );
 }
---
+++ src/GitHubUser/Spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className='min-w-full flex justify-center items-center h-20'>
+      <div className='animate-spin border-4 border-t-[#89d0ec] rounded-full w-10 h-10'></div>
+    </div>
+  );
+}
