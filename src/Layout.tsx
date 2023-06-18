76217d4a2dac1fc213b07fa509335a9f4d205306
Adrian Burgos <adrian.burgos.colas@gmail.com>
2023-06-18 06:16:51 -0400

    REFACTOR: added "Layout" comp to App comp

---
+++ src/Layout.tsx
@@ -0,0 +1,43 @@
+import Logo from './assets/github.svg';
+
+export default function Layout({children}:{children:React.ReactNode}) {
+  return (
+      <div className='flex flex-col min-h-[100vh] mx-auto max-w-screen sm:max-w-[32rem]'>
+        <header className='text-neutral-700'>
+          <div 
+            className={`
+              flex items-center justify-center gap-2 h-32 
+              bg-teal-100 mb-4 sm:mt-4 sm:rounded-lg shadow-lg
+            `}
+          >
+            <img src={Logo} width={60} height={60} alt='Logo' />
+            <h1 className='text-4xl font-black min-w-[210px]'>GitHub User</h1>
+          </div>
+          <p 
+            className='text-neutral-400 text-center text-sm w-fit m-auto p-4'
+          >
+            Welcome to Your GitHub Profile Information App!
+          </p>
+        </header>
+        <main className='grow'>
+          {children}
+        </main>
+        <footer className='bg-[#fbccd6] p-4 text-sm text-center'>
+          <div className='flex gap-2 w-fit mx-auto'>
+            <img src={Logo} width={20} height={20} alt='Logo' />
+            <h3 className='text-neutral-700'>Coded by{' '}
+            <a 
+              href='https://adrianburgoscolas.github.io/portfolio' 
+              rel='noopener noreferrer' 
+              target='_blank'
+              className='underline text-[#007dbe]'
+            >
+              Adrian Burgos
+            </a>
+            </h3>
+          </div>
+          <p className='text-neutral-400 text-xs mt-2'>GitHub Profile Information App!</p>
+        </footer>
+      </div>
+  );
+}
